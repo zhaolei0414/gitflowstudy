10c4a3fa855816ce2e82c21e412f1526966e3373
赵磊 <1403665068@qq.com>
2021-12-12 13:33:26 +0800

    init

---
+++ pages/message/message.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<text>{{msg}}</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				msg:'hello-uni'
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
